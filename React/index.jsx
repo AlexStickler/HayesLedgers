0cb845cd685bc2683e7dc47bee7c652e5876918c
Alex Stickler <astickler5@gmail.com>
2017-10-11 00:51:33 -0500

    Responsive Table
    Bootstrap things

--- React/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//component
-import Root from './components/root';
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Root/>, document.getElementById('root'));
-});
